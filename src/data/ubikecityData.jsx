8fc02b47cc7dba849661b8c97d18a46bdc266a43
tonia94731 <huiyu.tonia.chang@gmail.com>
2023-11-10 14:59:54 +0800

    feat: edit favicon

--- src/data/ubikecityData.jsx
+++ src/data/ubikecityData.jsx
@@ -4,10 +4,10 @@ export const taiwanCountiesAndCities = [
   { chinese: '桃園市', english: 'Taoyuan' },
   { chinese: '新竹市', english: 'Hsinchu' },
   { chinese: '新竹縣', english: 'Hsinchu-County' },
+  { chinese: '新竹科學園區', english: 'HSP' },
   { chinese: '苗栗縣', english: 'Miaoli-County' },
   { chinese: '台中市', english: 'Taichung' },
   { chinese: '嘉義市', english: 'Chiayi' },
-  { chinese: '嘉義縣', english: 'Chiayi-County' },
   { chinese: '台南市', english: 'Tainan' },
   { chinese: '高雄市', english: 'Kaohsiung' },
   { chinese: '屏東縣', english: 'Pingtung-County' }
