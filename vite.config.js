3ffcae8c8a2c4d32094b70922cd39234512bfe68
tonia94731 <huiyu.tonia.chang@gmail.com>
2023-11-21 15:03:53 +0800

    feat: add homepage

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,5 @@ import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom'] // Add external dependencies here
-    }
-  },
   base: '/YouBike/'
 })
